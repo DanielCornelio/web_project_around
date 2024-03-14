bf3e1923c2d97eb55d4be62e29b9962c4cccb9a5
Daniel Cornelio <jdcornelio89@gmail.com>
2024-03-13 23:26:05 -0600

    se agrega segundo formulario

--- scripts/index.js
+++ scripts/index.js
@@ -1,76 +1,56 @@
 // Busquemos el formulario en el DOM
+let coverProfile = document.querySelector(".popup_profile");
+let coverNewPlace = document.querySelector(".popup_new-place");
 let formElement = document.querySelector(".popup__container");
+// let formElementNewPlace = document.querySelector(".popup__container");
 let showModal = document.querySelector(".profile__edit-button");
+let showModalNewPlace = document.querySelector(".profile__add-button");
 let closeModal = document.querySelector(".popup__close");
+let closeModalNewPlace = document.querySelector("#closeNP");
+let profileName = document.querySelector(".profile__name");
+let nameInput = document.querySelector(".popup__input_name");
+let profileJob = document.querySelector(".profile__job");
+let jobInput = document.querySelector(".popup__input_job");
+
 function handleShowModal(evt) {
-  //prevenimos el refresh
   evt.preventDefault();
-  //definimos variables asignandole los elementos que emplearemos
-  let cover = document.querySelector(".popup");
-
-  let profileName = document.querySelector(".profile__name");
-  let profileJob = document.querySelector(".profile__job");
-
-  let nameInput = document.querySelector(".popup__input_title");
-  let jobInput = document.querySelector(".popup__input_link");
-
+  coverProfile.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+}
 
-  cover.classList.add("popup_opened");
-
-
+function handleShowModalNewPlace(evt) {
+  evt.preventDefault();
+  coverNewPlace.classList.add("popup_opened");
 }
 
 function handleCloseModal(evt) {
-  //prevenimos el refresh
   evt.preventDefault();
-  //definimos variables asignandole los elementos que emplearemos
-  let cover = document.querySelector(".popup");
-
-  cover.classList.remove("popup_opened");
-
-
+  coverProfile.classList.remove("popup_opened");
+}
+function handleCloseModalNewPlace(evt) {
+  evt.preventDefault();
+  coverNewPlace.classList.remove("popup_opened");
 }
 
-// Lo siguiente es el manipulador (handler) de entrega de formularios, aunque
-// no se enviará en ningún sitio todavía
-
-// Observa que el nombre de la función comienza con un verbo
-// y describe exactamente lo que hace la función
 function handleProfileFormSubmit(evt) {
-  // Esta línea impide que el navegador
-  // entregue el formulario en su forma predeterminada.
   evt.preventDefault();
-  // Una vez hecho esto, podemos definir nuestra propia forma de entregar el formulario.
-  // Lo explicaremos todo con más detalle después.
-
-  // Busquemos los campos del formulario en el DOM
-  let nameInput = formElement.querySelector(".popup__input_title");
-  let jobInput = formElement.querySelector(".popup__input_link");
-
-  // Obtén los valores de cada campo desde la propiedad de valor correspondiente
-  nameInput.value;
-  jobInput.value;
-
-
-  // Selecciona los elementos donde se introducirán los valores de los campos
-  let profileName = document.querySelector(".profile__name");
-  let profileJob = document.querySelector(".profile__job");
-
-  // Inserta nuevos valores utilizando el textContent
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  let cover = document.querySelector(".popup");
-
-  cover.classList.remove("popup_opened");
-
-  // propiedad del método querySelector()
+  coverProfile.classList.remove("popup_opened");
+}
+function handleNewPlaceFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  coverProfile.classList.remove("popup_opened");
 }
 
-// Conecta el manipulador (handler) al formulario:
-// se observará el evento de entrega
 showModal.addEventListener("click", handleShowModal);
+showModalNewPlace.addEventListener("click", handleShowModalNewPlace);
+
 closeModal.addEventListener("click", handleCloseModal);
+closeModalNewPlace.addEventListener("click", handleCloseModalNewPlace);
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
+// formElementNewPlace.addEventListener("submit", handleNewPlaceFormSubmit);
