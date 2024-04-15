a27f744cf4de5d8eb5fb7b0bbc2f897f9c38d39e
Daniel Cornelio <jdcornelio89@gmail.com>
2024-04-14 21:18:36 -0600

    se agrega validacion a profile

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,78 +1,51 @@
-const showInputError = (formElement, inputElement, errorMessage, settings) => {
-    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.add(settings.inputErrorClass);
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add(settings.errorClass);
-  };
-  
-  const hideInputError = (formElement, inputElement, settings) => {
-    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.remove(settings.inputErrorClass);
-    errorElement.classList.remove(settings.errorClass);
-    errorElement.textContent = "";
-  };
-  
-  const checkInputValidity = (formElement, inputElement, settings) => {
-    if (!inputElement.validity.valid) {
-      showInputError(
-        formElement,
-        inputElement,
-        inputElement.validationMessage,
-        settings
+function setEventListeners(form, formConfig) {
+  const formInputs = Array.from(
+    form.querySelectorAll(formConfig.inputSelector)
+  );
+  const submitButton = form.querySelector(formConfig.submitButtonSelector);
+
+  formInputs.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      const errorNode = form.querySelector(
+        `.popup__error_type_${inputElement.name}`
       );
-    } else {
-      hideInputError(formElement, inputElement, settings);
-    }
-  };
-  
-  const hasInvalidInput = (inputList) => {
-    return inputList.some((inputElement) => {
-      return !inputElement.validity.valid;
+      if (!inputElement.validity.valid) { 
+        inputElement.classList.add(formConfig.inputErrorClass);
+        console.log(inputElement)
+        console.log(inputElement.validationMessage)
+
+        errorNode.textContent = inputElement.validationMessage;
+        
+        } else {
+        inputElement.classList.remove(formConfig.inputErrorClass);
+       
+           errorNode.textContent = "";
+      }
+      submitButton.disabled = !isValidInputs(formInputs);
     });
-  };
-  
-  const toggleButtonState = (inputList, buttonElement, settings) => {
-    if (hasInvalidInput(inputList)) {
-      buttonElement.classList.add(settings.inactiveButtonClass);
-    } else {
-      buttonElement.classList.remove(settings.inactiveButtonClass);
-    }
-  };
-  
-  const setEventListeners = (formElement, settings) => {
-    const inputList = Array.from(
-      formElement.querySelectorAll(settings.inputSelector)
-    );
-    const buttonElement = formElement.querySelector(
-      settings.submitButtonSelector
-    );
-  
-    toggleButtonState(inputList, buttonElement, settings);
-  
-    inputList.forEach((inputElement) => {
-      inputElement.addEventListener("input", function () {
-        checkInputValidity(formElement, inputElement, settings);
-        toggleButtonState(inputList, buttonElement, settings);
-      });
-    });
-  };
-  
-  const enableValidation = (settings) => {
-    const formList = Array.from(document.querySelectorAll(settings.formSelector));
-    formList.forEach((formElement) => {
-      formElement.addEventListener("submit", function (evt) {
-        evt.preventDefault();
-      });
-  
-      setEventListeners(formElement, settings);
-    });
-  };
-  
-  enableValidation({
-    formSelector: ".popup__form",
-    inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__button",
-    inactiveButtonClass: "popup__button_disabled",
-    inputErrorClass: "popup__input_type_error",
-    errorClass: "popup__error_visible",
   });
+
+  submitButton.disabled = !isValidInputs(formInputs);
+}
+
+function enableValidation(formConfig) {
+  const forms = document.querySelectorAll(formConfig.formSelector);
+  forms.forEach((form) => {
+    setEventListeners(form, formConfig);
+  });
+}
+
+function isValidInputs(formInputs) {
+  return formInputs.every((item) => {
+    return item.validity.valid;
+  });
+}
+
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_inactive",
+  inputErrorClass: "popup__input_has_error",
+  errorClass: "popup__error",
+});
